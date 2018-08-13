de8cd25993932904b8cc0d20af9ea391dd05bff1
teoespero <tespero@wgu.edu>
2018-08-13 16:01:11 -0700

    final answer to exercise

--- exercise.js
+++ exercise.js
@@ -7,9 +7,18 @@
 // Bonus: Select the first div only and change its font color to pink
 
 if (jQuery){
-	$("div").css("background", "purple");
-	$(".highlight").css("fontSize", "200px");
-	$("#third").css("border", "2px solid orange");
+
+	// Select all divs and give them a purple background
+	$("div").css("background", "purple"); 
+
+	//select the divs with class "highlight" and make them 200px wide
+	$("div.highlight").css("width", "200px");
+
+	// select the dis with id "third" and give it an orange border
+	$("#third").css("border", "4px solid orange");
+
+	// Bonus: Select the first div only and change its font color to pink
+	$("div:first-of-type").css("color", "pink")
 }
 else {
 	alert("jQuery is not defined");	
